30329db24e81beb4b7aed4faee79ccedd8c7b5b5
Premtamang17 <102890974+Premtamang17@users.noreply.github.com>
2022-04-24 10:15:01 +0545

    Add files via upload

---
+++ BMI.js
@@ -0,0 +1,11 @@
+let prabinmass = 40;
+let prabinheight = 10;
+
+let rammass = 50;
+let ramheight = 40;
+
+let bmiprabin = (prabinmass / (prabinheight * prabinheight));
+console.log(bmiprabin);
+
+let bmiram = (rammass / (ramheight * ramheight));
+console.log(bmiram);
---
+++ assignment3.js
@@ -0,0 +1,15 @@
+bill = 400
+
+if (50 > bill < 300) {
+    tip = bill * 0.1
+
+
+
+} else {
+
+    tip = bill * 0.15
+
+}
+console.log(`the total amount is  ${bill} and tipping amount is ${tip}`);
+console.log(" therefore the total amount is"
+    bill + tip);
